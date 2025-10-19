2a55357e110303e81fe0e578578ee8170dad961b
MifKills <131961788+MifKills@users.noreply.github.com>
2025-10-19 16:36:32 +0200

    Beginner Series #3 Sum of Numbers

---
+++ 7kyu/Beginner Series #3 Sum of Numbers.py
@@ -0,0 +1,11 @@
+# My solution
+# def get_sum(a, b):
+#     return sum(range(a, b+1)) if a < b else sum(range(b, a+1))
+
+# Shorter way to make
+# def get_sum(a,b):
+#     return sum(range(min(a, b), max(a, b) + 1))
+
+# Math way to make
+# def get_sum(a, b):
+#     return (a + b) * (abs(a - b) + 1) // 2 # Formule: https://www.cuemath.com/sum-of-integers-formula/
