6db2c279655c2345f04b7b9387430dba9ff00ca2
MifKills <131961788+MifKills@users.noreply.github.com>
2025-10-25 18:08:14 +0200

    Strings Mix : add solution with Counter class from collections

--- 4kyu/Strings Mix.py
+++ 4kyu/Strings Mix.py
@@ -39,3 +39,24 @@
 #             which = "1" if val1 > val2 else "2" if val2 > val1 else "="
 #             hist[ch] = (-max_v1_v2, which + ":" + ch * max_v1_v2)
 #     return "/".join(hist[ch][1] for ch in sorted(hist, key=lambda x: hist[x]))
+
+
+# Solution with Counter object
+# from collections import Counter
+
+# def mix(s1, s2):
+#     count1 = Counter(filter(str.islower, s1))
+#     count2 = Counter(filter(str.islower, s2))
+
+#     parts = []
+#     for c in set(count1.keys() | count2.keys()):
+#         n1, n2 = count1.get(c, 0), count2.get(c, 0)
+#         if n1 > 1 or n2 > 1:
+#             if n1 > n2:
+#                 parts.append('1:' + c * n1)
+#             elif n1 < n2:
+#                 parts.append('2:' + c * n2)
+#             else:
+#                 parts.append('=:' + c * n1)
+
+#     return '/'.join(sorted(parts, key=lambda s: (-len(s), s)))
