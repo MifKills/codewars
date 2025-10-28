0503d5d18d210e6cc75279fd486a0e8289df6bc1
MifKills <131961788+MifKills@users.noreply.github.com>
2025-10-28 16:11:35 +0100

    Sudoku Solver : Bug in backtracking()

--- 3kyu/Sudoku Solver.py
+++ 3kyu/Sudoku Solver.py
@@ -1,15 +1,62 @@
 def sudoku(puzzle):
-    """return the solved puzzle as a 2d array of 9 x 9"""
-    return puzzle
+    def backtracking(state):
+        if is_solve(state):
+            return state
+
+        for row in range(9):
+            for col in range(9):
+                for num in range(9):
+                    if is_valid(state, row, col, num):
+                        state[row][col] = num
+                        backtracking(state)
+                        state[row][col] = 0
+    return backtracking(puzzle)
 
 
-def is_valid(state):
-    return None
+def set_number_in_puzzle(state, row, col, num):
+    copy_state = state.copy()
+    copy_state[row][col] = num
+    return copy_state
 
 
-def get_row(state, index):
-    return state[index]
+def is_solve(state):
+    for i in range(9):
+        row = get_number_in_row(state, i)
+        col = get_number_in_col(state, i)
+        for n in range(9):
+            if n not in row or n not in col:
+                return False
+    return True
 
 
-def get_col(state, index):
-    return list(row[index] for row in state)
+def is_valid(state, row, col, num):
+    new_state = set_number_in_puzzle(state, row, col, num)
+    for i in range(9):
+        row = get_number_in_row(new_state, i)
+        col = get_number_in_col(new_state, i)
+        for n in range(9):
+            if row.count(n) > 1 or col.count(n) > 1:
+                return False
+    return True
+
+
+def get_number_in_row(state, index):
+    return list(num for num in state[index] if num != 0)
+
+
+def get_number_in_col(state, index):
+    return list(num for num in list(row[index] for row in state) if num != 0)
+
+
+puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+          [6, 0, 0, 1, 9, 5, 0, 0, 0],
+          [0, 9, 8, 0, 0, 0, 0, 6, 0],
+          [8, 0, 0, 0, 6, 0, 0, 0, 3],
+          [4, 0, 0, 8, 0, 3, 0, 0, 1],
+          [7, 0, 0, 0, 2, 0, 0, 0, 6],
+          [0, 6, 0, 0, 0, 0, 2, 8, 0],
+          [0, 0, 0, 4, 1, 9, 0, 0, 5],
+          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+
+print(sudoku(puzzle))
