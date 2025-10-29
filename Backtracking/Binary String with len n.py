c289f859313a418dba2d6d635f894bf93802f52b
MifKills <131961788+MifKills@users.noreply.github.com>
2025-10-29 11:40:48 +0100

    Backtracking traning

---
+++ Backtracking/Binary String with len n.py
@@ -0,0 +1,40 @@
+# n = len of string
+def get_all_strings(n):
+    result = list()
+    backtracking("", n, result)
+    return result
+
+
+def backtracking(string: str, n: int, result: list):
+    if len(string) == n:
+        result.append(string)
+        return
+    for choice in range(2):
+        string += str(choice)
+        backtracking(string, n, result)
+        string = string[:-1]
+
+
+# Better version
+# def get_all_strings(n: int) -> list[str]:
+#     result: list[str] = []
+#     path: list[str] = []
+
+#     def backtrack():
+#         if len(path) == n:
+#             result.append("".join(path))
+#             return
+#         # выбор 0
+#         path.append('0')
+#         backtrack()
+#         path.pop()
+#         # выбор 1
+#         path.append('1')
+#         backtrack()
+#         path.pop()
+
+#     backtrack()
+#     return result
+
+
+print(get_all_strings(3))
